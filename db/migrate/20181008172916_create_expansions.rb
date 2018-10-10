420b4e3c7b28fa201aec363fe43012d8bb345785
Bradley Froese <bfroese777@gmail.com>
2018-10-10 14:41:29 +0000

    Added image model, forgot to do it with the rest, as well as some NOT NULLs on the database.

--- db/migrate/20181008172916_create_expansions.rb
+++ db/migrate/20181008172916_create_expansions.rb
@@ -1,10 +1,10 @@
 class CreateExpansions < ActiveRecord::Migration[5.2]
   def change
     create_table :expansions do |t|
-    	t.string 	:expansion_name
-    	t.string 	:short_code
-    	t.datetime 	:release_date
-    	t.string	:border
+    	t.string 	:expansion_name, null: false
+    	t.string 	:short_code, null: false
+    	t.datetime 	:release_date, null: false
+    	t.string	:border, null: false
 
       	t.timestamps
     end
--- db/migrate/20181010140030_create_types.rb
+++ db/migrate/20181010140030_create_types.rb
@@ -1,8 +1,8 @@
 class CreateTypes < ActiveRecord::Migration[5.2]
   def change
     create_table :types do |t|
-    	t.string :type_name
-    	t.string :category
+    	t.string :type_name, null: false
+    	t.string :category, null: false
 
       	t.timestamps
     end
