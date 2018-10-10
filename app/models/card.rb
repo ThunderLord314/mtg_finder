61bf9080668b13e7b141e4352e3128cb600a39e8
Bradley Froese <bfroese777@gmail.com>
2018-10-10 15:01:50 +0000

    Added associations on the models.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,6 @@
 class Card < ApplicationRecord
+	has_and_belongs_to_many :expansions
+	has_and_belongs_to_many :artists
+	has_and_belongs_to_many :types
+	has_many :images
 end
--- app/models/expansion.rb
+++ app/models/expansion.rb
@@ -1,2 +1,3 @@
 class Expansion < ApplicationRecord
+	has_and_belongs_to_many :cards
 end
