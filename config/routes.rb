f2a05222226702caa6e9cdca54c196dc73445fff
Bradley Froese <bfroese777@gmail.com>
2018-10-24 14:32:10 +0000

    Added a navigation bar, with links to a search page and about page, updated the show/id page to show associated data.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'cards' => 'cards#index'
+  root 'cards#index'
+
   get 'cards/:id', to: 'cards#show', id: /\d+/
+
+  get 'about' => 'about#index'
 end
