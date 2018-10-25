7468948985a38bdafb40efa7f04cf1e2c367c6a4
Bradley Froese <bfroese777@gmail.com>
2018-10-25 16:50:49 +0000

    Added navigation between cards, expansions, and artists based on the many-to-many associations they have.

--- app/controllers/expansions_controller.rb
+++ app/controllers/expansions_controller.rb
@@ -1,5 +1,5 @@
 class ExpansionsController < ApplicationController
 	def show
-		@expansion = Expansion.find(params[:id])
+		@expansion = Expansion.find(params[:id]).page(params[:page]).per(40)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'artists/:id', to: 'artists#show', id: /\d+/ 
 
   get 'expansions/:id', to: 'expansions#show', id: /\d+/
-
+ 
   get 'search' => 'search#index'
   get 'search' => 'search#show'
 
